60588903247887d7cc0e3d0f094933bd199c357d
xu <1>
2021-11-25 15:21:53 +0800

    i

--- start.sh
+++ start.sh
@@ -10,7 +10,9 @@
 #  --pretrained_dir /home/ubuntu/xu/cct_14_7x2_384_imagenet.pth
 
 
-python3 train.py -c main/ctfg_cub.yml \
-  --model ctfg_14_7x2_384 --data_dir /hy-nas/CUB2 \
-  --pretrained_dir /hy-nas/cct_14_7x2_384_imagenet.pth \
-  --is_con_los
+#python3 train.py -c main/ctfg_cub.yml \
+#  --model ctfg_14_7x2_384 --data_dir /hy-nas/CUB2 \
+#  --pretrained_dir /hy-nas/cct_14_7x2_384_imagenet.pth \
+#  --is_con_los
+
+nnictl create --config config_nni.yml
