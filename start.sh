9ad52b3feda3fd8816f1a55d10ddf41c46b1af50
xu <1>
2021-11-30 22:30:09 +0800

    v5.5.0

--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@
 #  --is_con_los --is_changeSize
 
 python3 -m torch.distributed.launch --nproc_per_node=2 \
-  train.py -c main/ctfg_nabirds.yml \
-  --model ctfg_14_7x2_384 --data_dir /home/ubuntu/xu/nabirds2 \
+  train.py -c main/ctfg_cub.yml \
+  --model ctfg_14_7x2_384 --data_dir /home/ubuntu/xu/CUB2 \
   --pretrained_dir /home/ubuntu/xu/cct_14_7x2_384_imagenet.pth \
   --is_con_loss
 
