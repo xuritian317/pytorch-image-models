681a151c10c14fbd078d38252a3cfe2e6ad30903
xu <1>
2021-12-04 19:22:24 +0800

    v5.7.0

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 #nnictl create --config config_nni.yml
 
-python3 train.py -c main/ctfg_cars.yml \
-  --model ctfg_14_7x2_384 --data_dir /hy-nas/cars2 \
-  --pretrained_dir /hy-nas/cct_14_7x2_384_imagenet.pth \
+python3 train.py -c main/ctfg_cub.yml \
+  --model ctfg_14_7x2_384 --data_dir /hy-nas/CUB2 \
   --is_con_los
+
+#  --pretrained_dir /hy-nas/cct_14_7x2_384_imagenet.pth \
 
 #CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=2 \
 #  train.py -c main/ctfg_nabirds.yml \
 #  --model ctfg_14_7x2_384 --data_dir /home/lirunze/xh/datas/nabirds2 \
 #  --pretrained_dir /home/lirunze/xh/datas/cct_14_7x2_384_imagenet.pth \
-#  --is_con_loss --apex-amp
+#  --is_con_loss
 
 #CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=2 \
 #  train.py -c main/ctfg_cub.yml \
@@ -18,8 +19,12 @@ python3 train.py -c main/ctfg_cars.yml \
 #  --is_con_loss
 
 #python3 -m torch.distributed.launch --nproc_per_node=2 \
-#  train.py -c main/ctfg_dogs.yml \
-#  --data_dir /home/ubuntu/xu/dogs2 \
+#  train.py -c main/ctfg_imagenet.yml \
+#  --data_dir /home/ubuntu/xu/btf2 \
 #  --pretrained_dir /home/ubuntu/xu/cct_14_7x2_384_imagenet.pth \
 #  --is_con_loss
 
+#python3 -m torch.distributed.launch --nproc_per_node=2 \
+#  train.py -c main/ctfg_imagenet.yml \
+#  --data_dir /home/ubuntu/Projects/MobileViT-main/data/imagenet_data \
+#  --is_con_loss
